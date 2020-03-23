bef18fce7af5b92653202894e7aaaa844cb6790c
Kabir Kanjani <andrewkabir@gmail.com>
2020-03-23 16:59:35 +0530

    First Commit

---
+++ src/main/java/Movie/KMovies.java
@@ -0,0 +1,16 @@
+package Movie;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+ public class KMovies {
+   /*     public static void main(String[] args) {
+            ApiContextInitializer.init();
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+            try {
+                telegramBotsApi.registerBot( new getmovies());
+                 } catch (TelegramApiRequestException e) {
+            }
+        }*/
+    }
